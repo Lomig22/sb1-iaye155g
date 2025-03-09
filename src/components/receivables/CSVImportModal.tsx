3899a84cb15aee7036074f663a51493d335ff19a
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2025-03-09 22:14:26 +1100

    Delete lines on import

--- src/components/receivables/CSVImportModal.tsx
+++ src/components/receivables/CSVImportModal.tsx
@@ -7,6 +7,7 @@ import Papa from 'papaparse';
 interface CSVImportModalProps {
 	onClose: () => void;
 	onImportSuccess: (importedCount: number) => void;
+	receivables: (Receivable & { client: Client })[];
 }
 // Shanaka (Start)
 const columnMapping: { [key: string]: string } = {
@@ -114,6 +115,7 @@ const columnMapping: { [key: string]: string } = {
 export default function CSVImportModal({
 	onClose,
 	onImportSuccess,
+	receivables,
 }: CSVImportModalProps) {
 	const [file, setFile] = useState<File | null>(null);
 	const [data, setData] = useState<any[]>([]);
@@ -834,6 +836,31 @@ export default function CSVImportModal({
 						.in('id', clientIds);
 				} catch (err) {
 					console.error('Erreur lors de la mise à jour des clients:', err);
+				}
+			}
+			// Delete lines that were not in the csv
+			const prevItems = new Set(
+				receivables.map((item) => `${item.owner_id}-${item.invoice_number}`)
+			);
+
+			const missingReceivables = receivables.filter(
+				(item) => !prevItems.has(`${item.owner_id}-${item.invoice_number}`)
+			);
+
+			if (missingReceivables.length > 0) {
+				try {
+					await supabase
+						.from('receivables')
+						.delete()
+						.in(
+							'id',
+							missingReceivables.map((item) => item.id)
+						);
+				} catch (err) {
+					console.error(
+						'Erreur lors de la suppression des créances manquantes:',
+						err
+					);
 				}
 			}
 
